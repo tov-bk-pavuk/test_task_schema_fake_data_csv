b2cfcb971b114eeead35792db6e1b3e5ffff97cb
master <tov.bk.pavuk@gmail.com>
2021-10-29 05:43:57 +0300

    авторизация частично припилена

--- u_auth/views.py
+++ u_auth/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.success(request, ('Wrong password or username'))
+            return redirect('u_login')
+    return render(request, 'u_auth/login.html')
+
