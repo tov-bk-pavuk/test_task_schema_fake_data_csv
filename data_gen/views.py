fd401e2799cb5b209b3ab722fd6dfccab1b11d08
master <tov.bk.pavuk@gmail.com>
2021-10-30 07:08:14 +0300

    структура готова

--- data_gen/views.py
+++ data_gen/views.py
@@ -2,11 +2,11 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
-#@login_required(login_url='/users/u_login')
+@login_required(login_url='/users/u_login')
 def data_sets(request):
     return render(request, 'data_gen/data_sets.html')
 
 
-#@login_required(login_url='/users/u_login')
+@login_required(login_url='/users/u_login')
 def data_gen(request):
     pass
--- schemas/views.py
+++ schemas/views.py
@@ -2,11 +2,11 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
-#@login_required(login_url='/users/u_login')
+@login_required(login_url='/users/u_login')
 def home(request):  # Schemas list
     return render(request, 'schemas/home_data_schemas.html')
 
 
-#@login_required(login_url='/users/u_login')
+@login_required(login_url='/users/u_login')
 def new_schema(request):
     return render(request, 'schemas/new_schema.html')
