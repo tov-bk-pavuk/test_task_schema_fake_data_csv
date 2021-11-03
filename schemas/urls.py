408beacb89ddd62c349c1b5cc53b6c8cb1c5d978
master <tov.bk.pavuk@gmail.com>
2021-11-03 09:56:30 +0200

    перед редактированием связей в моделях

--- schemas/urls.py
+++ schemas/urls.py
@@ -5,4 +5,7 @@ from schemas import views
 
 urlpatterns = [
     path('', views.new_schema, name='new_schema'),
+    path('create_cls_bsd', views.SchemaCreateView.as_view(), name='new_schema'),
+    path('update_cls_bsd/<int:id>', views.SchemaUpdateView.as_view(), name='upd_schema'),
+    path('delete_cls_bsd/<int:id>', views.SchemaDeleteView.as_view(), name='dlt_schema'),
 ]
--- schemas/views.py
+++ schemas/views.py
@@ -1,13 +1,45 @@
 from django.contrib.auth.decorators import login_required
+from django.forms import inlineformset_factory
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import (ListView,
+                                  CreateView,
+                                  UpdateView,
+                                  UpdateView,
+                                  DeleteView)
+from django.views.generic.list import MultipleObjectMixin
 
-from . models import Schema
+from .models import Schema, Column
 
 
 class SchemasListView(ListView):
     model = Schema
     template_name = 'schemas/home_data_schemas.html'
+    fields = ['name', 'separator', 'string_character', 'column']
+
+
+class SchemaCreateView(MultipleObjectMixin, CreateView):
+    # SchemeFormSet = inlineformset_factory(Schema, Column)
+    model = Schema
+    template_name = 'schemas/new_schema_cls_bsd.html'
+    fields = ['name', 'separator', 'string_character', 'column']
+    success_url = '/'
+    object_list = Column.objects.all()
+    # extra_context = Column.objects.all()
+
+
+class SchemaUpdateView(UpdateView):
+    model = Schema
+    pk_url_kwarg = 'id'
+    template_name = 'schemas/new_schema_cls_bsd.html'
+    fields = ['name', 'separator', 'string_character', 'column']
+    success_url = '/'
+
+
+class SchemaDeleteView(DeleteView):
+    model = Schema
+    pk_url_kwarg = 'id'
+    template_name = 'schemas/del_obj.html'
+    success_url = '/'
 
 
 @login_required(login_url='/users/u_login')
