84183fe50b6e0667432ca7c54b8f65c4d1996fa7
master <tov.bk.pavuk@gmail.com>
2021-11-07 09:16:57 +0200

    разнесён функционал генерации данных

--- data_gen/views.py
+++ data_gen/views.py
@@ -28,42 +28,23 @@ class DataSetListView(ListView):
     template_name = 'data_gen/data_sets.html'
 
 
+class SchemaDataSetListView(ListView):
+    model = DataSetFile
+    template_name = 'data_gen/data_sets.html'
+
+
 @login_required(login_url='/users/u_login')
 def data_gen(request):
     # Create the HttpResponse object with the appropriate CSV header.
-    id_pk = 4  # Временные переменные нужно поулчать из ссылки
+    id_pk = 1  # Временные переменные нужно поулчать из ссылки
     amount = 1000  # Временные переменные
 
     name = models.Schema.objects.get(pk=id_pk).name
-    # response = HttpResponse(
-    #     content_type='text/csv',
-    #     headers={'Content-Disposition': f'attachment; filename="{name}.csv"'},
-    # )
 
     sep = models.Schema.objects.get(pk=id_pk).separator
     str_char = models.Schema.objects.get(pk=id_pk).string_character
     columns = models.Schema.objects.get(pk=id_pk).column_set.all().order_by('order', 'name')
 
-    fake = Faker()
-
-    def col_type(a: str):  # Функция определяет тип поля для Faker-a
-        if a == 'FN':
-            return fake.name()
-        elif a == 'JB':
-            return fake.job()
-        elif a == 'EM':
-            return fake.ascii_email()
-        elif a == 'DN':
-            return fake.domain_name()
-        elif a == 'PN':
-            return fake.phone_number()
-        elif a == 'CN':
-            return fake.company()
-
-    # writer = csv.writer(response, delimiter=sep, quotechar=str_char)
-    # writer.writerow([f'{columns[i]}' for i in range(len(columns))])
-    # for _ in range(amount):
-    #     writer.writerow([f'{col_type(columns[i].field_type)}' for i in range(len(columns))])
     DataSetFile.objects.get_or_create(url=f'static/media/{name}.csv')
 
     with open(f'static/media/{name}.csv', 'w') as f:
@@ -71,8 +52,25 @@ def data_gen(request):
         writer.writerow([f'{columns[i]}' for i in range(len(columns))])
         for _ in range(amount):
             writer.writerow([f'{col_type(columns[i].field_type)}' for i in range(len(columns))])
-        # myfile = File(f)
-        # myfile.write(str(response))
+            # col_type определён ниже
 
     # messages.success(request, ('Данные генерируются'))
     return HttpResponseRedirect(reverse('data_sets'))  # render(request, 'data_gen/data_sets.html')
+
+
+fake = Faker()
+
+
+def col_type(a: str):  # Функция определяет тип поля для Faker-a
+    if a == 'FN':
+        return fake.name()
+    elif a == 'JB':
+        return fake.job()
+    elif a == 'EM':
+        return fake.ascii_email()
+    elif a == 'DN':
+        return fake.domain_name()
+    elif a == 'PN':
+        return fake.phone_number()
+    elif a == 'CN':
+        return fake.company()
