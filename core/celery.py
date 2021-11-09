fae1e23c6ba55720f9d8ef59e3bdff104a5fde53
master <tov.bk.pavuk@gmail.com>
2021-11-09 14:22:41 +0200

    prod_settings

--- core/celery.py
+++ core/celery.py
@@ -3,7 +3,7 @@ import os
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.local_settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.prod_settings')
 
 app = Celery('core')
 
